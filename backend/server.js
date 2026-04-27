25f3ceb975ba94fa90309085922b7e5d2cdff517
Brody Roth <brodyroth23@gmail.com>
2026-04-26 22:51:50 -0400

    Adding new shorternUrl, getUrl, and deleteUrl functions in controllers. Also adding prettier and linter to improve project look and code maintenence.

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,41 @@
 require('dotenv').config();
 const { createServer } = require('node:http');
-const { shortenUrl, getUrl, deleteUrl } = require('./controllers/controllers.js');
+const {
+  shortenUrl,
+  getUrl,
+  deleteUrl,
+} = require('./controllers/controllers.js');
 
 const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 
 const server = createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'application/json');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
 
-    try {
-        if (request.method === 'POST' && request.url === '/shorten') {
-            shortenUrl(request, response);
-        }
-        if (request.method === 'GET' && request.url.startsWith('/')) {
-            getUrl(request, response);
-        }
-        if (request.method === 'DELETE' && request.url.startsWith('/')) {
-            deleteUrl(request, response);
-        } else {
-            response.statusCode = 404;
-            response.end(JSON.stringify({ error: 'Not Found' }));
-        }
+  try {
+    if (request.method === 'POST' && request.url === '/shorten') {
+      shortenUrl(request, response);
     }
-    catch (error) {
-        console.error('Error processing request:', error);
-        response.statusCode = 500;
-        response.end(JSON.stringify({ error: 'Internal Server Error' }));
+    if (request.method === 'GET' && request.url.startsWith('/')) {
+      getUrl(request, response);
     }
+    if (request.method === 'DELETE' && request.url.startsWith('/')) {
+      deleteUrl(request, response);
+    } else {
+      response.statusCode = 404;
+      response.end(JSON.stringify({ error: 'Not Found' }));
+    }
+  } catch (error) {
+    console.error('Error processing request:', error);
+    response.statusCode = 500;
+    response.end(JSON.stringify({ error: 'Internal Server Error' }));
+  }
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -4,8 +4,32 @@ const { nanoid } = require('nanoid');
 
 const prisma = new PrismaClient();
 
-async function shortenURL(originalUrl) {};
+async function shortenURL(originalUrl) {
+  const shortCode = nanoid(10);
 
-async function getURL(shortCode) {};
+  const url = await prisma.url.create({
+    data: {
+      shortCode: shortCode,
+      originalUrl: originalUrl,
+    },
+  });
+  return url;
+}
 
-async function deleteURL(shortCode) {};
+async function getURL(shortCode) {
+  const originalUrl = await prisma.url.findUnique({
+    where: {
+      shortCode: shortCode,
+    },
+  });
+  return originalUrl;
+}
+
+async function deleteURL(shortCode) {
+  await prisma.url.delete({
+    where: {
+      shortCode: shortCode,
+    },
+  });
+  return shortCode;
+}
